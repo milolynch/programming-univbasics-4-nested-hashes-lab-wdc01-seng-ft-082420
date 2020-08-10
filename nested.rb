658aa22ceeb5df78295e5a439b051db56111888a
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-10 21:35:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,7 +88,6 @@ end
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -104,7 +103,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  programmer_hash[:alan_kay][:known_for] = 'GUI
   programmer_hash
 end
 
